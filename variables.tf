1bb6ea0c6d041aa0eb0a19e296c663c11f4e73c6
acodealfred <65242796+acodealfred@users.noreply.github.com>
2021-05-09 00:30:02 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -32,3 +32,28 @@ variable "key_name" {
 	type = string
 	default = "poc_admin_key"
 }
+
+variable "poc_vpc_cidr"{
+	type = string
+	default = "172.16.0.0/24"
+}
+
+variable "poc_sub_net_1" {
+	type = string
+	defaut = "172.16.0.0/26"
+}
+
+variable "poc_sub_net_2" {
+	type = string
+	defaut = "172.16.0.64/26"
+}
+
+variable "poc_sub_net_3" {
+	type = string
+	defaut = "172.16.0.128/26"
+}
+
+variable "poc_sub_net_4" {
+	type = string
+	defaut = "172.16.0.192/26"
+}
