4a224b308c09c309158cd03d6c3a75c8f0f46a9d
acodealfred <65242796+acodealfred@users.noreply.github.com>
2021-05-08 19:17:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,22 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+provider "random" {}
+
+resource "random_pet" "name" {}
+
+
+resource "aws_instance" "web" {
+  ami = "ami-02f26adf094f51167"
+  instance_type = "t2.micro"
+ # user_data = file("init-script.sh")
+
+
+  tags = {
+    Name = random_pet.name.id
+  }
+}
+
+
 
