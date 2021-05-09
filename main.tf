46cd435fe2883a7064645791befc753834558140
acodealfred <65242796+acodealfred@users.noreply.github.com>
2021-05-09 19:27:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -123,8 +123,8 @@ resource "aws_security_group" "allow_ssh_tls" {
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    cidr_blocks      = "0.0.0.0/0"
+    
   }
 
   egress {
