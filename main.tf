7356b438580a4e9041ea2e1d3b7df58f1050c64f
acodealfred <65242796+acodealfred@users.noreply.github.com>
2021-05-09 20:34:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -160,6 +160,19 @@ resource "aws_nat_gateway" "nat_gw" {
 }
 
 
+resource "aws_route_table" "pri_rt" {
+  vpc_id = aws_vpc.poc_demo.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gw.id
+  }
+
+  tags = {
+    Name = "PRIVATE_RT_TABLE"
+  }
+
+}
 
 
 #Lauch AWS-EC2 in VPC-public subnet
