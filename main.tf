953f67e65317a671eec24c67637919c0a1d7546d
acodealfred <65242796+acodealfred@users.noreply.github.com>
2021-05-09 19:24:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -102,3 +102,40 @@ resource "aws_route_table" "pri_rt" {
 
 }
 */
+
+# Public subnet security group
+resource "aws_security_group" "allow_ssh_tls" {
+  name        = "allow_ssh_tls"
+  description = "Allow SSH/TLS inbound traffic"
+  vpc_id      = aws_vpc.poc_demo.id
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = "0.0.0.0/0"
+    
+  }
+
+  ingress {
+    description      = "TLS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "Allow SSH-LTS"
+  }
+}
