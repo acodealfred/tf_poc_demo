cdbfed4e5d985d473d917b5b4ebde9621e9a5678
acodealfred <65242796+acodealfred@users.noreply.github.com>
2021-05-09 16:49:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -54,3 +54,46 @@ resource "aws_subnet" "poc_subnet_private_ap-se1" {
         Name = "poc_subnet_private_ap-se1"
     }
 }
+
+#Add reduntant subnet
+
+#Adding Inetenet GW to public subnet
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.poc_demo.id
+
+  tags = {
+    Name = "POC_IGW"
+  }
+}
+
+#Add Route table for Public subnet
+resource "aws_route_table" "pub_rt" {
+  vpc_id = aws_vpc.poc_demo.id
+
+  route {
+    cidr_block = "172.16.0.0/26"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+    tags = {
+    Name = "PUBLIC_RT_TABLE"
+  }
+
+
+}
+
+#Add Route table for private subnet
+resource "aws_route_table" "pri_rt" {
+  vpc_id = aws_vpc.poc_demo.id
+
+  route {
+    cidr_block = "172.16.0.64/26"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "PRIVATE_RT_TABLE"
+  }
+
+}
