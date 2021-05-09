ba7bf92a6581616cb6697928fcd58b97ad64d772
acodealfred <65242796+acodealfred@users.noreply.github.com>
2021-05-09 11:54:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,18 +6,30 @@ provider "random" {}
 
 resource "random_pet" "name" {}
 
-
+/*
+--EC2 in default VPC
 resource "aws_instance" "web" {
   #ami = "ami-02f26adf094f51167"
   ami = var.aws_ami
   #instance_type = "t2.micro"
   instance_type = var.instance_type
   user_data = file("post-installs.sh")
-
- key_name = var.key_name
+  key_name = var.key_name
 
   tags = {
     Name = random_pet.name.id
+  }
+}
+*/
+
+#POC Demo VPC Creation
+
+resource "aws_vpc" "poc_demo" {
+  cidr_block       = var.poc_vpc_cidr
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "poc_demo"
   }
 }
 
